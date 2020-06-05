538c60ec3d2b4310b50df57be8c2fc6da3dce07c
Christian Ruiz <santacruz.crm@gmail.com>
2020-06-04 22:59:26 -0700

    Add final player file

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,5 @@
-
 class Player
-    attr_reader :color, :choices
-
-    BLACK_PIECES = ["\u{2657}", "\u{2654}", "\u{2658}", "\u{2659}", "\u{2655}", "\u{2656}"]
-    WHITE_PIECES = ["\u{265D}", "\u{265A}", "\u{265E}", "\u{265F}", "\u{265B}", "\u{265C}"]
+    attr_reader :color
     
     def initialize(color)
         @color = color
